ef6d16f63c485a9e6dd20acbbd4351a3f2809841
Bianca Maciel <bianca.maciel@climatempo.com.br>
2020-05-02 13:39:22 -0300

    Implementação dos botões remover e adicionar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
+
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+   const result = await api.post('repositories', {
+     title: `Novo Titulo ${new Date()}`,
+     url: 'http://github.com.br/BiancaMaciel',
+     techs: '[Node, Reactjs, React-native]'
+   });
+   
+   const repository = result.data;
+   setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const result = await api.delete(`repositories/${id}`)
+    const newRepositories = repositories.filter(repository => repository.id !== id );    
+    setRepositories(newRepositories);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
+      { repositories.map(repository =>
+        <li key={repository.id}>{repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>  
         </li>
+      )}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
